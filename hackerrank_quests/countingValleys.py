1797366001c4586873d9f775406ecad722d461d3
eemektas <eemektas@gmail.com>
2020-02-14 17:12:25 +0300

    to be cont

--- hackerrank_quests/countingValleys.py
+++ hackerrank_quests/countingValleys.py
@@ -1,23 +1,24 @@
 n = 0
-s = 'DUUDDUUDDUUDDUUDD'
+s = 'UDDDUDUU'
 
 ls = [s[i] for i in range(len(s))]
 
-att, att2, level = 0, 0, 0
+att, level = 0, 0
 
-for step in ls:
-	if step == 'U':
-		att += 1
-	elif step == 'D':
-		att -= 1
-	if att < 0:
-		level += 1
-	if level > 0 and att > 0:
-		att2 = 1
-	else:
-		att2 == 0
-	if att2 > 0 and att < 0:
-		level += 1
+for step in range(len(ls)):
+	if ls[att:step+1].count('D') + ls[att:step+1].count('U') == 0:
+		att = step
+		if ls[att:step+1].count('D') > ls[att:step+1].count('U'):
+			level += 1 
+
+	# if att < 0:
+	# 	level += 1
+	# if level > 0 and att > 0:
+	# 	att2 = 1
+	# else:
+	# 	att2 == 0
+	# if att2 > 0 and att < 0:
+	# 	level += 1
 		
 
 print(level)
