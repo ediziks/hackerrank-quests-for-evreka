ba92552c5efb31205c581854c7e079c6bad9aac1
eemektas <eemektas@gmail.com>
2020-02-17 13:04:43 +0300

    solution

---
+++ hackerrank_quests/hourglassSum.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the hourglassSum function below.
+def hourglassSum(arr):    
+    result = [] 
+    for i in range(6):
+        for j in range(6):
+            try:
+                hGlass = arr[i][j] + arr[i][j+1] + arr[i][j+2] 
+                hGlass += arr[i+1][j+1] 
+                hGlass += arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2]
+                result.append(hGlass)
+            except:
+                pass
+            
+    return max(result)
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr = []
+
+    for _ in range(6):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = hourglassSum(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
