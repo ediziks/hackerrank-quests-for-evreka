fc85613238b767d53635da935ab34ccfd7d2d2ef
eemektas <eemektas@gmail.com>
2020-03-06 01:55:37 +0300

    prettification

--- hackerrank_quests/sherlockAndAnagrams.py
+++ hackerrank_quests/sherlockAndAnagrams.py
@@ -7,14 +7,15 @@ import re
 import sys
 from collections import Counter
 
+
 # Complete the sherlockAndAnagrams function below.
 def sherlockAndAnagrams(s):
     count = 0
-    for i in range(1,len(s)+1):
-        a = ["".join(sorted(s[j:j+i])) for j in range(len(s)-i+1)]
+    for i in range(1, len(s) + 1):
+        a = ["".join(sorted(s[j:j + i])) for j in range(len(s) - i + 1)]
         b = Counter(a)
         for j in b:
-            count+=b[j]*(b[j]-1)/2
+            count += b[j] * (b[j] - 1) / 2
     return int(count)
     
 if __name__ == '__main__':
